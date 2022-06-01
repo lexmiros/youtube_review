b0603eb3b29b82a1e528e67145f0af4cc9326013
Lex Miros <aleksei.miros@outlook.com>
2022-06-01 13:44:35 +1000

    Cleaning imports

--- src/run.py
+++ src/run.py
@@ -1,17 +1,5 @@
 from GetData import *
-import os
-import requests
-from dotenv import load_dotenv
-from googleapiclient.discovery import build
-from bs4 import BeautifulSoup, Tag
 
-load_dotenv()
-
-#Building variables
-api_key = os.getenv("API_KEY")
-service_name = 'youtube'
-version = 'v3'
-youtube = build(service_name, version, developerKey=api_key)
 
 if __name__ == "__main__":
     print(get_channel_stats("CreatedTechOfficial"))
